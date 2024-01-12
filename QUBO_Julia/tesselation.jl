72476e87560e0551d3363cf4e7954016ee11a138
AMANDA MELO <ammelo@id.uff.br>
2024-01-12 08:27:43 -0300

    translation of tesselation, 3sat formula not working

--- QUBO_Julia/tesselation.jl
+++ QUBO_Julia/tesselation.jl
@@ -16,11 +16,11 @@ function r1(nodes:: Int64 , edges:: Vector{Vector{Int64}} , n_tess:: Int64, poss
                 a2 = possible_edges[a2_index]
                 if has_common(a1,a2)
                     #Penalidade por escolher 2 arestas em comum
-                    Q[init+a1_index][init+a2_index] = p
-                    Q[init+a2_index][init+a1_index] = p
+                    Q[init+a1_index][init+a2_index] = p/2
+                    Q[init+a2_index][init+a1_index] = p/2
                 else
-                    Q[init+a1_index][init+a2_index] = -p
-                    Q[init+a2_index][init+a1_index] = -p
+                    Q[init+a1_index][init+a2_index] = -p/2
+                    Q[init+a2_index][init+a1_index] = -p/2
                 end
             end
         end
@@ -32,18 +32,27 @@ function r1(nodes:: Int64 , edges:: Vector{Vector{Int64}} , n_tess:: Int64, poss
             pos_e1 = findfirst(item -> item == (x,y),possible_edges)
             pos_e2 = findfirst(item -> item == (x,z),possible_edges)
             pos_e3 = findfirst(item -> item == (y,z),possible_edges)
-            e1_signal = (edges[x][y] == 1) ? 1 : -1
-            e2_signal = (edges[x][z] == 1) ? 1 : -1
-            e3_signal = (edges[y][z] == 1) ? 1 : -1
+            e1_signal = (edges[x][y] == 1) ? -1 : 1
+            e2_signal = (edges[x][z] == 1) ? -1 : 1
+            e3_signal = (edges[y][z] == 1) ? -1 : 1
             #Penalidade por escolher todos os 3 vértices
-            Q[a_init+a_index][a_init+a_index] = p
+            Q[a_init+a_index][a_init+a_index] = 1
             #Penalidade para cada aresta da tripla
-            Q[a_init+a_index][init+pos_e2] = p * e1_signal
-            Q[a_init+a_index][init+pos_e1] = p * e2_signal
-            Q[a_init+a_index][init+pos_e3] = p * e3_signal
-            Q[init+pos_e1][a_init+a_index] = p * e1_signal
-            Q[init+pos_e2][a_init+a_index] = p * e2_signal
-            Q[init+pos_e3][a_init+a_index] = p * e3_signal
+            # Q[a_init+a_index][init+pos_e2] = -2 
+            # Q[a_init+a_index][init+pos_e1] = -2 
+            # Q[a_init+a_index][init+pos_e3] = 1
+            Q[init+pos_e1][a_init+a_index] = -2 
+            Q[init+pos_e2][a_init+a_index] = -2 
+            Q[init+pos_e3][a_init+a_index] = 1 
+            #Penalidade para cada par de aresta
+            Q[init+pos_e3][init+pos_e3] -= 1
+            Q[init+pos_e1][init+pos_e2] += 2
+            Q[init+pos_e2][init+pos_e1] += 2
+            # Q[init+pos_e1][init+pos_e3] = p/2
+            # Q[init+pos_e2][init+pos_e3] = p/2
+            # Q[init+pos_e3][init+pos_e1] = p/2
+            # Q[init+pos_e3][init+pos_e2] = p/2
+            
         end
     end
 end
@@ -57,14 +66,14 @@ function r2(nodes:: Int64 , edges:: Vector{Vector{Int64}} , n_tess:: Int64, poss
                 index = findfirst(item -> item == (line,column),possible_edges)
                 if edges[line][column] == 0
                     # Penalidade por escolher não existente
-                    Q[init+index][init+index] = p
+                    Q[init+index][init+index] += p
                 else
-                    Q[init+index][init+index] = -p
+                    Q[init+index][init+index] += -p
                     for t_aux = 1:n_tess
                         if t_aux != t_index
                             t_init = (t_aux-1) * edges_vars
                             #Penalidade por escolher existente em alguma tesselação
-                            Q[init+index][t_init+index] = p/2
+                            Q[init+index][t_init+index] += p/2
                         end
                     end
                 end
@@ -82,18 +91,26 @@ function Q(nodes:: Int64 , edges:: Vector{Vector{Int64}} , n_tess:: Int64)
     size = n_tess * (edges_vars + ancilla_vars)
 
     Q = [[0 for _ = 1:size] for _ = 1:size]
-    _r1 = r1(nodes, edges, n_tess, possible_edges,possible_ancilla, Q)
     _r2 = r2(nodes, edges, n_tess, possible_edges,possible_ancilla, Q)
+    _r1 = r1(nodes, edges, n_tess, possible_edges,possible_ancilla, Q)
     return reduce(hcat, Q)',size
 end
 
 nodes = 4
+#square with diagonal (1,3) and not (2,4)
 edges = [
     [0,1,1,1],
     [1,0,1,0],
-    [0,1,0,1],
+    [1,1,0,1],
     [1,0,1,0],
 ]
+# square
+# edges = [
+#     [0,1,0,1],
+#     [1,0,1,0],
+#     [0,1,0,1],
+#     [1,0,1,0],
+# ]
 n_tess = 2
 
 q, vars = Q(nodes,edges,n_tess)
@@ -111,3 +128,12 @@ end
 
 xi = value.(x)
 show(xi)
+println()
+possible_edges = [(x,y) for x =1:nodes for y =x+1:nodes]
+edges_vars = length(possible_edges)
+show(possible_edges)
+for i=1:(edges_vars*n_tess)
+    if xi[i] == 1.0
+        println("Edge ", possible_edges[i - (edges_vars*div(i-1,edges_vars))], " in tesselation ", 1+div(i,(edges_vars+1)))
+    end
+end

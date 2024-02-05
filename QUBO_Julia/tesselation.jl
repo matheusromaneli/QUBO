74b7db746353990e9ac2dc6eb90568c38314aff1
Matheus Lima Romaneli <matheusromaneli@id.uff.br>
2024-02-05 10:14:59 -0300

    fix values and ancillia bits

--- QUBO_Julia/tesselation.jl
+++ QUBO_Julia/tesselation.jl
@@ -4,7 +4,7 @@ using QUBODrivers
 using BenchmarkTools
 using .Utils
 
-p = 4
+p = 2
 
 function r1(nodes:: Int64 , edges:: Vector{Vector{Int64}} , n_tess:: Int64, possible_edges::Vector{Tuple{Int64, Int64}},possible_ancilla::Vector{Tuple{Int64, Int64, Int64}}, Q::Vector{Vector{Int64}})
     edges_vars = length(possible_edges)
@@ -12,36 +12,45 @@ function r1(nodes:: Int64 , edges:: Vector{Vector{Int64}} , n_tess:: Int64, poss
         init = (t_index-1) * edges_vars
         ancilla_init = n_tess*edges_vars
         ancilla_vars = length(possible_ancilla)
-        a_init = ancilla_init + (t_index-1)*ancilla_vars
+        a_init = ancilla_init + (t_index-1)*ancilla_vars*3
         for a_index =1:ancilla_vars
-            (x,y,z) = possible_ancilla[a_index]
-            pos_e1 = findfirst(item -> item == (x,y),possible_edges)
-            pos_e2 = findfirst(item -> item == (x,z),possible_edges)
-            pos_e3 = findfirst(item -> item == (y,z),possible_edges)
-            # Penalidade auxiliar para a variável a mais
-            Q[init+pos_e1][init+pos_e2] += p/2
-            Q[init+pos_e1][a_init+a_index] += -2*p
-            Q[init+pos_e2][a_init+a_index] += -2*p
-            Q[a_init+a_index][a_init+a_index] = 3*p
-            # Escolher 2 arestas apenas
-            Q[init+pos_e1][init+pos_e2] += p/2
-            Q[init+pos_e1][init+pos_e3] += p/2
-            Q[init+pos_e2][init+pos_e3] += p/2
+            # Apply restriction: 
+            #  2*a*b + 2*a*c + 2*b*c 
+            # -2*a*x - 2*b*x + 3*x 
+            # -2*a*y - 2*c*y + 3*y 
+            # -2*b*z - 2*c*z + 3*z 
+            # -a*z - b*y - c*x
+            (a,b,c) = possible_ancilla[a_index]
+            #Index of my ancilla bits
+            x_index = a_init + (a_index-1)*3 + 1
+            y_index = a_init + (a_index-1)*3 + 2
+            z_index = a_init + (a_index-1)*3 + 3
+            #Index of my edges based on a, b, c variables
+            pos_e1 = init+findfirst(item -> item == (a,b),possible_edges)
+            pos_e2 = init+findfirst(item -> item == (a,c),possible_edges)
+            pos_e3 = init+findfirst(item -> item == (b,c),possible_edges)
             # Escolher par com inexistente
-            if edges[x][y] == 0
-                Q[init+pos_e1][init+pos_e2] += p
-                Q[init+pos_e1][init+pos_e3] += p
-            end
-            if edges[x][z] == 0
-                Q[init+pos_e1][init+pos_e2] += p
-                Q[init+pos_e2][init+pos_e3] += p
-            end
-            if edges[y][z] == 0
-                Q[init+pos_e2][init+pos_e3] += p
-                Q[init+pos_e1][init+pos_e3] += p
-            end
+            Q[pos_e1][pos_e2] += 2*p
+            Q[pos_e1][pos_e3] += 2*p
+            Q[pos_e2][pos_e3] += 2*p
+            # Penalidade auxiliar para a variável a mais x
+            Q[pos_e1][x_index] += -2*p
+            Q[pos_e2][x_index] += -2*p
+            Q[x_index][x_index] = 3*p
+            # Penalidade auxiliar para a variável a mais y
+            Q[pos_e1][y_index] += -2*p
+            Q[pos_e3][y_index] += -2*p
+            Q[y_index][y_index] = 3*p
+            # Penalidade auxiliar para a variável a mais z
+            Q[pos_e2][z_index] += -2*p
+            Q[pos_e3][z_index] += -2*p
+            Q[z_index][z_index] = 3*p
             #Penalidade por escolher todas as arestas
-            Q[init+pos_e3][a_init+a_index] += -3*p
+            Q[pos_e1][z_index] += -p
+            Q[pos_e2][y_index] += -p
+            Q[pos_e3][x_index] += -p
+            #Auxiliar da seleção de todas arestas
+            #TODO: testar como se fosse C(A(1-B)) para não haver termos unicos 
         end
     end
 end
@@ -55,7 +64,7 @@ function r2(nodes:: Int64 , edges:: Vector{Vector{Int64}} , n_tess:: Int64, poss
                 index = findfirst(item -> item == (line,column),possible_edges)
                 if edges[line][column] == 0
                     # Penalidade por escolher não existente
-                    Q[init+index][init+index] += p
+                    Q[init+index][init+index] += 2*p
                 else
                     # Penalidade por escolher existente
                     Q[init+index][init+index] += -p
@@ -78,7 +87,7 @@ function Q(nodes:: Int64 , edges:: Vector{Vector{Int64}} , n_tess:: Int64)
     possible_ancilla = [(x,y,z) for x =1:nodes for y =1x+1:nodes for z = y+1:nodes]
     ancilla_vars = length(possible_ancilla)
     
-    size = n_tess * (edges_vars + ancilla_vars)
+    size = n_tess * (edges_vars + ancilla_vars*3)
 
     Q = [[0 for _ = 1:size] for _ = 1:size]
     _r2 = r2(nodes, edges, n_tess, possible_edges,possible_ancilla, Q)
@@ -87,19 +96,18 @@ function Q(nodes:: Int64 , edges:: Vector{Vector{Int64}} , n_tess:: Int64)
 end
 
 # nodes = 3
-# #square with diagonal (1,3) and not (2,4)
 # edges = [
-#     [0,1,0],
-#     [1,0,0],
+#     [0,0,0],
+#     [0,0,0],
 #     [0,0,0],
 # ]
 nodes = 4
-#square with diagonal (1,3) and not (2,4)
+# square with diagonal (1,3) and not (2,4)
 edges = [
     [0,1,1,1],
-    [1,0,1,0],
+    [1,0,1,1],
     [1,1,0,1],
-    [1,0,1,0],
+    [1,1,1,0],
 ]
 # square
 # edges = [
@@ -108,7 +116,7 @@ edges = [
 #     [0,1,0,1],
 #     [1,0,1,0],
 # ]
-n_tess = 2
+n_tess = 1
 
 q, vars = Q(nodes,edges,n_tess)
 
